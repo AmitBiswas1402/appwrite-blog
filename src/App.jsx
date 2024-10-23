dd4909534e88618e404ea5b809671dc8d2175b09
Amit Biswas <amit.142biswas@gmail.com>
2024-10-23 20:29:00 +0530

    production components

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
-      Hello world<Outlet />
+      <Outlet />
     </h1>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
-
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
@@ -76,7 +76,7 @@ const router = createBrowserRouter([
   }
 ])
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />
